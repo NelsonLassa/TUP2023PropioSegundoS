e919906a37495aeb9698f3da6e946ff29244070a
Lassa <nelsonlassa63@gmail.com>
2023-08-28 01:15:13 -0300

    Listas y tuplas

--- Python/Leccion 1/main.py
+++ Python/Leccion 1/main.py
@@ -13,19 +13,19 @@ print(nombres[0:2])  # el intervalo a mostrar es del 0 al 2 pero no incluye el 2
 print(nombres[:3])  # muestra del inicio sin incluir el valor 3
 print(nombres[1:])  # muestra del valor 1 hasta el ultimo
 
-#Modificamos un valor
+# Modificamos un valor
 
-nombres[3]= "Papá"
+nombres[3] = "Papá"
 print(nombres)
 
-#Iterar una lista
-for nombre in nombres: # nombre es singular, la lista esta en plurarl
-    print(nombre)
+# Iterar una lista
+for i in nombres:  # nombre es singular, la lista esta en plurarl
+    print(i)
 else:
     print("Se acabaron los elementos de la lista")
 
 # Preguntamos cuantos elementos tiene la lista
-print(len(nombres)) # pasamos como parametro la lista
+print(len(nombres))  # pasamos como parametro la lista
 
 # Agregamos un elemento
 nombres.append("Sofi")
@@ -46,13 +46,48 @@ nombres.pop()
 print(nombres)
 
 # Eliminar un indice especifico
-del nombres[1] # del significa delete (eliminar)
+del nombres[1]  # del significa delete (eliminar)
 print(nombres)
 
 # Eliminar, borrar o limpiar todos los elementos
 nombres.clear()
-print(nombres) # borra el contenido
+print(nombres)  # borra el contenido
 
 # Eliminar lista
 del nombres
+"""
 print(nombres) # este borra la variable nombre
+"""
+
+# Definimos una tupla
+cocina = ("cuchara", "cuchillo", "tenedor")
+print(cocina)
+# Obtenemos la cantidad de elementos que la contienen
+print(len(cocina))
+
+# Acceder a un elemento, para esto utilizamos corchetes no parentesis
+print(cocina[0])
+
+# mostrar de manera inversa
+print(cocina[-1])
+
+# Acceder a un rango
+print(cocina[0:2])
+
+# Ejemplo
+verdura = ("papa",)  # Una tupla necesita aunque sea un elemento la coma
+
+# Recorremos los elemtos de la tupla
+
+for cocinar in cocina:
+    print(cocinar)  # de forma vertical con salto de linea \n  -> alt+92 para salto de linea
+for cocinar in cocina:
+    print(cocinar, end=" ")  # de forma horizontal o en una misma linea
+
+cocinaLista = list(cocina)
+cocinaLista[0] = "plato"
+cocina = tuple(cocinaLista)
+print("\n", cocina)
+
+# del cocina
+# esto se utiliza para eliminar la tupla
