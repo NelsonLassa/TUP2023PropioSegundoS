d6bde9e98cc668aab5f113eddc32838ed5711599
Lassa <nelsonlassa63@gmail.com>
2023-10-25 01:01:08 -0300

    clase4 javascript

---
+++ JavaScript/lección3/js/script.js
@@ -0,0 +1,21 @@
+// Tipos de Datos en JavaScript
+/*
+La sintaxis en lo que es comenterios
+es muy similar a la de Java 
+realmente diriamos es es identica
+*/
+var nombre = "Nelson"; // Tipo Str
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
+var numero = 3000; //Tipo Númerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Nelson",
+    apellido : "Lassa",
+    telefono : "26404621012"
+}
+console.log(objeto);
