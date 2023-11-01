71e2ae91dcaafbe114fa43b68c2199c0c011b577
Lassa <nelsonlassa63@gmail.com>
2023-11-01 01:53:17 -0300

    Clase 10 y modificaciones de la clase 9

--- JavaScript/leccion8/js/Script.js
+++ JavaScript/leccion8/js/Script.js
@@ -1,72 +1,115 @@
-// Ejercicio 1: Calcular estacion del año
-let mes = 4;
-let estacionAnio;
+// Ejercicio para encontrar números pares
+let numero = 10;
+if (numero % 2 == 0) {
+  console.log("El número es par:" + numero);
+} else {
+  console.log("El número es impar:" + numero);
+}
 
-if(mes == 1 || mes == 2 || mes == 12){
-  estacionAnio="Verano";
+// Ejercicio: es mayor de edad
+let mayorMenor = 18;
+if (mayorMenor >= 18) {
+  console.log(
+    "La persona es mayor de edad porque tiene " + mayorMenor + " años"
+  );
+} else {
+  console.log(
+    "La persona es menor de edad porque tiene " + mayorMenor + " años"
+  );
 }
-else if(mes == 4|| mes == 5 || mes == 6){
-  estacionAnio="Otoño";
-}
-else if(mes == 6 || mes == 7 || mes == 8){
-  estacionAnio="Invierno";
-}
-else if(mes == 9 || mes == 10 || mes == 11){
-  estacionAnio="Primavera";
-}
-else{
-  estacionAnio ="No existe ese mes";
-}
-console.log("La estacion del año es: "+estacionAnio)
 
-// Ejercicio 2: Hora del dia
-let horaDia = 10;
-let accionDia ;
+// Ejercicio: Dentro de un rango
+let dentroRango = 11;
+let valMin = 0,
+  valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax) {
+  console.log("Esta dentro del rango establecido");
+} else {
+  console.log("Esta fuera del rango establecido");
+}
 
-if (horaDia>=0 && horaDia<=8){
-  accionDia = "Durmiendo para poder reponer energia";
-}
-else if (horaDia>8 && horaDia<=9){
-  accionDia = "Perpararse para ir a trabajar";
-}
-else if (horaDia>9 && horaDia<=13){
-  accionDia = "Trabajando";
-}
-else if (horaDia>13 && horaDia<=14){
-  accionDia = "Almuerzo";
-}
-else if (horaDia>14 && horaDia<=17){
-  accionDia = "Continuar trabajando";
-}
-else if (horaDia>17 && horaDia<=18){
-  accionDia = "Llegar a casa y perparar la merienda";
-}
-else if (horaDia>18 && horaDia<=21){
-  accionDia = "Leer y comenzar con el cursado";
-}
-else if (horaDia>22 && horaDia<=24){
-  accionDia = "Leer y comenzar con el cursado";
-}
-else{
-  accionDia = "Esa no es una hora correcta" ;
-}
-console.log("Que estoy haciendo ?"+accionDia);
+// Ampliamos el uso de var let y const
+/*
+Con var puedes reasignar en cualquier momento
+este forma parte del ambito global
+Un error es que se sobreescriba
+ */
 
-// Estructura switch(la sintaxis es igual a java)
-switch(mes){
-  case 1: case 2: case 12:
-    estacionAnio="verano";
-    break
-  case 3: case 4: case 5:
-    estacionAnio="otoño";
-    break
-  case 6: case 7: case 8:
-    estacionAnio="Invierno";
-    break
-  case 9: case 10: case 11:
-    estacionAnio="Verano";
-    break
-  default: 
-    estacionAnio = "Valor incorrecto"
+var nombre = "Nelson";
+nombre = "Ezequiel";
+console.log(nombre);
+
+function saludar() {
+  var nombre3 = "Lassa";
+  console.log(nombre3);
 }
-console.log("Bienvenido a la estación de: "+estacionAnio);
+//console.log(nombre3); // Aqui no lee el dato en la funcion
+
+if (true) {
+  var edad = 34;
+  console.log(edad);
+}
+console.log(edad); // En la funcion funciono correctamente la estructura if fallo
+
+/*
+let :  esta puede ser reasifnada en cualquier momento
+la diferencia es que su ambito es del bloque,
+solo disponible dentro de un bloque de llaves
+o dentro de una función
+ */
+
+function saludar() {
+  let nombre2 = "Nelson";
+  console.log(nombre2);
+}
+console.log(nombre);
+if (true) {
+  let edad2 = 33;
+  console.log(edad2);
+}
+//console.log(edad2);
+/* 
+const se utiliza para valores constantes que no pueden ser reasignados
+ */
+
+const fechaNacimiento = 2006;
+console.log(fechaNacimiento);
+//fechaNacimiento = 2003;
+console.log(fechaNacimiento);
+
+// Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true,
+  diaDescanso = true;
+if (vacaciones || diaDescanso) {
+  console.log("El padre puede asistir al juego de su hijo");
+} else {
+  console.log("El padre no puede asistir al juego de su hijo");
+}
+
+// Operador ternario
+let resultado2 = 1 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2);
+let numero1 = 9;
+resultado2 = numero1 % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resultado2);
+
+// Convertir String a Numero
+let miNumero = "18";
+console.log(typeof miNumero);
+let edad3 = Number(miNumero); // ESto es una funcion
+console.log(typeof edad3);
+
+// Funcion isNan
+if (isNaN(edad3)) {
+  // No es un número = is not a Number(devuelve un resultado boleano)
+  console.log("Esta Variable no contiene solo numeros");
+} else {
+  if (edad3 >= 18) {
+    console.log("Pede votar");
+  } else {
+    console.log("Muy joven para votar");
+  }
+}
+
+let resultado3 = edad3 >= 18 ? "Pede votar" : "Muy joven para votar";
+console.log(resultado3);
